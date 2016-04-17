91a5afcd305b2058b52fb5a4b9403e7dd22fb324
Kleftodimos Alexandros <kleftodimos@gmail.com>
2016-04-17 12:54:50 +0300

    Added files via upload

--- display-videos/start.php
+++ display-videos/start.php
@@ -8,11 +8,11 @@ header("location:main_login.php");}
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
 <html>
 <head>
-<title>Νέες Τεχνολογίες της Επικοινωνίας- Βίντεο</title>
+<title>Video Examples</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-7">
 </head>
 
-<frameset rows="80,*" cols="*" framespacing="0" frameborder="yes" border="0" bordercolor="#CCCCCC">
+<frameset rows="150,*" cols="*" framespacing="0" frameborder="yes" border="0" bordercolor="#CCCCCC">
   <frame src="imageframe.htm" name="topFrame" scrolling="NO" noresize >
   <frame src="main-frame.php" name="mainFrame">
 </frameset>
